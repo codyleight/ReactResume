3a40e78bbdbff45056576405a9c103590d8aea2c
Cody Thompson <codylthomp@gmail.com>
2024-01-21 19:47:11 -0600

    picture fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ export default function HomePage() {
     <div className="bgda text-danger mt-5 home" style={{ display: 'flex', justifyContent: 'center' }}>
       
   <img
-    src="../assets/banner6.png"
+    src="https://lh3.googleusercontent.com/pw/ABLVV86kWQGNTkNeUzKDyvnCDl-77gilzCFZzGShMc85sw3VUq5WZ7x0SUuA1sg65zGysHYEp4Mpi6ctZ8pS3v4cmCrmunHwC4FGsGuM3pVV8cm2eKu1rZFYmytoQDc0yGA0QZL0ocie9O2YumYX2Ur274_S=w1875-h625-s-no-gm?authuser=0"
     alt="Full Stack Developer"
     className="img-fluid"
   />
