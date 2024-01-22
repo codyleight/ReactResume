eabe8d2c0eb09b1a957d9b8de258de788a0b1bc7
Cody Thompson <codylthomp@gmail.com>
2024-01-21 19:39:27 -0600

    Banner fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ export default function HomePage() {
     <div className="bgda text-danger mt-5 home" style={{ display: 'flex', justifyContent: 'center' }}>
       
   <img
-    src="./assets/banner6"
+    src="../assets/banner6.png"
     alt="Full Stack Developer"
     className="img-fluid"
   />
