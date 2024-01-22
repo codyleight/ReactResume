1583373103cd1db88b89a918e4f461ba3a50e60a
Cody Thompson <codylthomp@gmail.com>
2024-01-22 11:37:33 -0600

    Added new pictures, added a portfolio component

---
+++ src/components/Project.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Project = ({ title, imageSrc, link }) => {
+    return (
+      <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 text-center">
+        <Link className="text-light glow-on-hover" to={link}>
+          {title}
+        </Link>
+        <Link to={link}>
+          <img
+            src={imageSrc}
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt={title}
+          />
+        </Link>
+      </div>
+    );
+  };
+
+export default Project;
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,63 +1,18 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import Project from '../components/Project';
 
 export default function PortfolioPage() {
-  
-
   return (
-
-   
-  
-    <div className="bg text-primary d-flex justify-content-center" >
-       
-
-      <div class="row">
-
-  <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-    <h3 class="text-center">Project 1</h3>
-    <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-      class="w-100 shadow-1-strong rounded mb-4"
-      alt="Boat on Calm Water"
-    />
-<h3 class="text-center">Project 2</h3>
-    <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-      class="w-100 shadow-1-strong rounded mb-4"
-      alt="Wintry Mountain Landscape"
-    />
-  </div>
-
-  <div class="col-lg-4 mb-4 mb-lg-0">
-  <h3 class="text-center">Project 3</h3>
-    <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-      class="w-100 shadow-1-strong rounded mb-4"
-      alt="Mountains in the Clouds"
-    />
- <h3 class="text-center">Project 4</h3>
-    <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-      class="w-100 shadow-1-strong rounded mb-4"
-      alt="Boat on Calm Water"
-    />
-  </div>
-
-  <div class="col-lg-4 mb-4 mb-lg-0">
-  <h3 class="text-center">Project 5</h3>
-    <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
-      class="w-100 shadow-1-strong rounded mb-4"
-      alt="Waves at Sea"
-    />
- <h3 class="text-center">Project 6</h3>
-    <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
-      class="w-100 shadow-1-strong rounded mb-4"
-      alt="Yosemite National Park"
-    />
-  </div>
-</div>
+    <div className=" d-flex justify-content-center">
+      <div className="row">
+        <Project title="DnD Character Generator" imageSrc="src\assets\DND.png" link="https://codyleight.github.io/DnDCharacter/" />
+        <Project title="Project 2" imageSrc="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
+        <Project title="Project 3" imageSrc="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
+        <Project title="Project 4" imageSrc="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
+        <Project title="Project 5" imageSrc="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp" />
+        <Project title="Project 6" imageSrc="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp" />
+      </div>
     </div>
   );
 }
