a271aae509844f81eac11eb9630b0a09d4fd43e0
Cody Thompson <codylthomp@gmail.com>
2024-01-22 11:39:54 -0600

    Pushed assets into public folder

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -6,7 +6,7 @@ export default function PortfolioPage() {
   return (
     <div className=" d-flex justify-content-center">
       <div className="row">
-        <Project title="DnD Character Generator" imageSrc="src\assets\DND.png" link="https://codyleight.github.io/DnDCharacter/" />
+        <Project title="DnD Character Generator" imageSrc="public\assets\DND.png" link="https://codyleight.github.io/DnDCharacter/" />
         <Project title="Project 2" imageSrc="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
         <Project title="Project 3" imageSrc="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
         <Project title="Project 4" imageSrc="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
