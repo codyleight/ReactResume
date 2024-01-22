50ef07730f53d77e4e4df62e79163e13455aa4ad
Cody Thompson <codylthomp@gmail.com>
2024-01-22 13:15:21 -0600

    Picture edited

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,7 +16,7 @@ export default function HomePage() {
       
     </div>
     <div className="col-sm-5 align-self-center ">
-      <img src="https://lh3.googleusercontent.com/pw/ABLVV87lUjxpyy_2uFtp8gCOBaa-8pWoQJQ5U-F7pGC6TQxCol-BQnvFJJZNhOkBTLoyR4ws91XEAb_J-5T33b2y7Rx9yLmfkLv3hqVSCZvEY9RxjwzWohxBHoqAL0v9VuW70zCI3GhN9ea1NbZqObqLWIjmGVKDBcu8MsOSLpFZ-8m2m6Ue_hX2pjE2lbWtHFvLlWi4VSwq-94CKCCRFNh7wEA02_hXyHUEBBH1xhzZSvYvXr3z-pbw4Hj--WXnVR3tJYifH5z5tOjbkTnZTIKih55e2kTpMDnBQ2QR-WWTb0rX1AfxqR14lL1Ab4z866B3Fn8nueBZQQuhT0L4l3TIXfdW6lbsU_UTuBZt4MQQODtjCQYJOTdgQkDJeiz5cBZftA0bpBZDnh0l4Qm8peCqxn8-ptqnRulQ4j_j8pUnJCBFjUelbP3ocKy_ve73fM387YLwI_6z55rVp5Bx5oYroYXKnvwL2ibtLotcMSeB9_pQYwxDWgMx_wAqQn9HMdxC7Z4wqHH_ZClUvKxKWEUS3XzAWO6sGcwCd-BZXsL3P8fRwqtQiHk3PpljapwYuKNl3G7yHDkUEd6xOt0gxJlEJGh70nDQYlnucJotpTBx05IDV2WJLkFa1ozAQlnoYfG5djucOVVLVq9ZqQQubMQCD0gInbFl0y6h3MylR5YVzRio93_BmzpNuJwQD5cZXgM4CMsm5m4lgraS0wdX65_WfLEzFR6_THOT4HbugVi_GTWtUsnbpe_SRGQsyYU_XQ0KNcOhPTrvCk0LwQrT4agDllASyqX2KDPnT5a8U2X5lyDT9Mzekymf7OoWXRqgKSoHdLj7oMOpcpheEtK-B77Yu6-0RZ-2QtzjDeiKKIuXViCLP2mP2hp7t1RGbn38Z0f_NTDCVCa_FYxToAXMgi21op3b9YE9fzO1urSuTMKy1-cw8Utnt_lEglad_vzvvJ9kugTIUZ6-aw=w919-h919-s-no-gm?authuser=0" alt="Full Stack Developer" className="img-fluid" />
+      Cody Thompson
     </div>
     <div className="col-sm align-self-right d-flex justify-content-start ">
       <h1 className=''>Developing for the future</h1>
