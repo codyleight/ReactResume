980dab9b5590c237a097220ce23c11a932b7aa4f
Cody Thompson <codylthomp@gmail.com>
2024-01-23 11:54:57 -0600

    Added Language logos that I know to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,26 @@
-import { bottom } from '@popperjs/core';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+
+import { IoLogoJavascript } from "react-icons/io";
+import { FaReact, FaNode, FaUnity, FaJava } from "react-icons/fa";
+import { SiMysql, SiCsharp } from "react-icons/si";
+import { AiOutlineConsoleSql } from "react-icons/ai";
+import { TbFileTypeCss } from "react-icons/tb";
+import { FaHtml5 } from "react-icons/fa";
 
 export default function HomePage() {
   return (
 
 
-<div className=' '>
-  
-
-    <div className="row d-flex justify-content-center">
-      <div className="col-sm align-self ">
-        
-      </div>
-      <div className=" d-flex justify-content-center ">
-        
-        <img src="/assets/BannerFinal.png"  class="col-sm-12 img-fluid" alt="Banner Image"/>
-      </div>
-    
-    </div>
-    
-  
+<div className="container">
+  <div className="row">
+ 
+    <div className="col-12 d-flex justify-content-center">
+      <div className='iconsize text-light'><IoLogoJavascript /> <FaReact /> <FaNode /> <SiMysql /> <SiCsharp /> <FaUnity /> <AiOutlineConsoleSql /> <TbFileTypeCss /> <FaJava /> <FaHtml5 /></div>
+      
+      <img src="/assets/BannerFinal.png" className="img-fluid " alt="Banner Image" />
+      <div className="iconsize text-light">
+  <FaHtml5 /> <FaJava /> <TbFileTypeCss /> <AiOutlineConsoleSql /> <FaUnity /> <SiCsharp /> <SiMysql /> <FaNode /> <FaReact /> <IoLogoJavascript />
+</div> </div>
+  </div>
 </div>
 
 
